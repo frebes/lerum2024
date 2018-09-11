4c9c7b88ff6bc5a257efdd171d18e8dc7cd947a6
Gasta Design <sqs@gasta.no>
2018-09-11 09:18:03 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 	  	var pName = window.location.pathname,
 	  	    fName = pName.slice(1,-1);
 
-	  	if(fName === ''){ // If emptystr set id due to SQS class
+	  	if(fName === ''){ // Set first page lerum
 	    	$('.Site').addClass('forside');
 	  	}
 	 	else{
