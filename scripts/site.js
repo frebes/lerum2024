da869194b1d671858ca17f8b0cf14476f3e51fe2
Gasta Design <sqs@gasta.no>
2018-09-11 10:47:34 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
 	$(function(){
 		var galleryblock = $('.forside .Index-gallery-item-content');
-		gallery.parent().after(galleryblock);
+		galleryblock.parent().after(galleryblock);
 
 	});
 
