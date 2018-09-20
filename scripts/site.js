c87459da488ce234c0d14a7149ee69c6ce814a81
Gasta Design <sqs@gasta.no>
2018-09-20 13:46:17 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
 		var summaryInfo = $('.oppskrifter #oppskrift-toppgalleri .summary-excerpt p');
 
 		summaryInfo.each(function(){
-		    $(this).replaceWith('<div>Gå til oppskrifta</div>')
+		    $(this).replaceWith('<p>Gå til oppskrifta</p>')
 		});
 
 	});
