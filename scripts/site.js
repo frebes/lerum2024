0d36c09061e37db807186d99658c13fb8fa65be9
Gasta Design <sqs@gasta.no>
2018-09-20 13:11:20 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -46,12 +46,12 @@ $(document).ready(function(){
 
 	// Replace summary info
 
-	$(function(){
+//	$(function(){
 		var summaryInfo = $('.oppskrifter .summary-excerpt'),
 			sumContent = $('<div>Gå til oppskrifta</div>');
 
-		$('.oppskrifter .summary-excerpt').html(sumContent);
-	});
+		summaryInfo.html(sumContent);
+//
 
 	// Scroll header function
 
