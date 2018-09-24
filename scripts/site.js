1a570fa469a875e72582214134a7dc3490406617
Gasta Design <sqs@gasta.no>
2018-09-24 12:27:56 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 
 		textElem.hide();
 		hoverElem.hover(function(){
-			$(this).stop().next().find('.summary-title').slideToggle(200);
+			$(this).find('.summary-title').slideToggle(200);
 		});	
 	});
 
