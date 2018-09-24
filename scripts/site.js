3477b55ccc996d938af31fbf0cd375c3b4eb16aa
Gasta Design <sqs@gasta.no>
2018-09-24 13:43:21 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -29,9 +29,9 @@ $(document).ready(function(){
 		//	$(this).next().find('.summary-title').fadeIn();
 		hoverElem.hover(function(){
 			$(this).next().find('.summary-title').fadeIn();
-			//if (textElem.is(':hover'))
-			//	textElem.show();
-			//	return false;
+			if (textElem.is(':hover'))
+				textElem.stop(true);
+				return false;
 		}, function(event){
 			$('.summary-title').fadeOut();
 		});
