4d1d9cca35834a688dfb28598088f3e1564eb4e3
Gasta Design <sqs@gasta.no>
2018-09-24 14:00:01 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 
 		textElem.hide();
 		hoverElem.hover(function(){
-			$(this).next().find('#freistande-oppskrifter .summary-title').fadeIn();
+			$(this).next().find('.summary-title').fadeIn();
 		}, function(){
 			if (textElem.is(':hover')){
 				alert('test');
