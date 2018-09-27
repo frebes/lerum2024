9c33bf7ff12ed56122ab2249969736dc0915d09f
Gasta Design <sqs@gasta.no>
2018-09-27 08:14:43 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -110,7 +110,7 @@ $(document).ready(function(){
 	    	 $(this).nextUntil("h2").slideToggle();
 	     	 $(this).toggleClass('ui-closed ui-open');
 	  	});
-	  	$('<div>LUKK</div>').insertAfter('.markdown-block .sqs-block-content p');
+	  	//$('<div>LUKK</div>').insertAfter('.markdown-block .sqs-block-content p');
   	});
 
 
