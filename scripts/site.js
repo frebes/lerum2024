bf37f0131614cb9c5000c6156a615b16e20f4430
Gasta Design <sqs@gasta.no>
2018-10-09 12:30:41 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -150,8 +150,8 @@ $(document).ready(function(){
 
 		var url = location.pathname; // = location.href
 		var parts = location.href.split('/').slice(3);
-		parts[0] = 'Forside ';
-		var breadcrumb = parts.join(' &gt; ');
+		parts[0] = 'Forside / ';
+		var breadcrumb = parts.join(' / ');
 		$('.bc').html(breadcrumb);
 
   	});
