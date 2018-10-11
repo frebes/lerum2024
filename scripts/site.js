af7e75eda1d2b8d810098fd211aee864c7d49b86
Gasta Design <sqs@gasta.no>
2018-10-11 08:18:39 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
 		if ($(window).width() > 641){
 			
 		    $('#freistande-oppskrifter .summary-thumbnail').hover(function(){
-		    	alert('test');
+		    	//alert('test');
 		        $(this).find('#freistande-oppskrifter .summary-title').stop(true, true).fadeIn(300);
 		    }, function() {
 		        $(this).find('#freistande-oppskrifter .summary-title').stop(true, true).fadeOut(100);
