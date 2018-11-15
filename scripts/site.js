817742704f812d91c0434b9661fcb07c0f54e638
Gasta Design <sqs@gasta.no>
2018-11-15 12:00:21 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -173,7 +173,9 @@ $(document).ready(function(){
   			bc_omoss = $('.Site.om-lerum'),
   			bc_kontakt = $('.Site.kontakt-oss-innhold');
  
-
+ 		var parts = location.href.split('/').slice(3);	
+		parts[0] = '<a href="/">Forside</a> ';
+		var breadcrumb = parts.join(' / ');
 
   		if (bc_produkt[0])
   			bc.append('&nbsp;/&nbsp;<a href="/produkt/">Produkt</a>');
