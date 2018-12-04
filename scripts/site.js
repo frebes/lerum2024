6c1fc4057c51ffd6d51f909db04061cb8ba6e0b5
Gasta Design <sqs@gasta.no>
2018-12-04 08:47:43 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
 	  	var pName = window.location.pathname,
 	  	    fName = pName.slice(1,-1);
 
+	  	alert('fName')
 	  	if(fName === ''){ // Set first page lerum
 	    	$('.Site').addClass('forside');
 	  	}
