2bbb89a3a6bef008d7ab5c4d27cdb96495a9c9dd
Gasta Design <sqs@gasta.no>
2018-12-10 15:08:50 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -277,10 +277,10 @@ $(document).ready(function(){
   		if (produrl == 'syltety')
   			bc.append(htmls + '<a href="/produkt/">Produkt</a>' + htmls + '<a href="/produkt-syltetoy/">Syltetøy</a>');
 
-    	if (bc_prod_saft[0]){
+    	if (bc_prod_saft[0])
   			bc.append(htmls + '<a href="/produkt-saft/">Saft</a>');	
-	  		if (produrl == 'saft')
-	  			bc.append(htmls + '<a href="/produkt-saft/">Saft</a>');
+	  	if (produrl == 'saft')
+	  		bc.append(htmls '<a href="/produkt/">Produkt</a>' + htmls + '<a href="/produkt-saft/">Saft</a>');
   		}
 
     	if (bc_prod_jus[0])
