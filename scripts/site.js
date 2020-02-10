10752cbc1470e6d68736b0d6678381a121291b7b
Gasta Design <sqs@gasta.no>
2020-02-10 09:36:40 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -320,7 +320,7 @@ $(document).ready(function(){
   	$(function(){
 
   		$('.produktinnhold-ny .slide:first-child .image-slide-title').html('<a href="/smudi">Smudi</a>');
-  		$('.produktinnhold-ny .slide:nth-child(2) .image-slide-title').html('<a href="/brus">Brus</a>');
+  		$('.produktinnhold-ny .slide:nth-child(2) .image-slide-title').html('<a href="/produkt-brus">Brus</a>');
   		$('.produktinnhold-ny .slide:nth-child(3) .image-slide-title').html('<a href="/juleprodukt">Juleprodukt</a>');
   		$('.produktinnhold-ny .slide:nth-child(4) .image-slide-title').html('<a href="/produkt-storhusholdning">Storhusholdning</a>');
   		$('.produktinnhold-ny .slide:nth-child(5) .image-slide-title').html('<a href="/produkt-jus-og-nektar">Jus</a>');
