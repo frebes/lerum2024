d76523d79be6b65d3746e9949d09cce49dfd6c1a
Gasta Design <sqs@gasta.no>
2020-12-10 10:15:29 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -191,7 +191,7 @@ $(document).ready(function(){
 				galRep = $($('#fokusomrade .image-slide-title')[index]);
 
 			//console.log(sumLink);
-		    galRep.replaceWith(<a href="'+ galLink +'">bak</a>);
+		    galRep.replaceWith('<a href="'+ galLink +'">bak</a>');
 		});
 
 	});
